b8426a6b91a97bd779aef5c9ce027faf9658593c
Fara De Visscher <48617748+pbuukjgyh@users.noreply.github.com>
2024-03-09 12:26:55 +0100

    showed basic graph

--- Minigin/GraphSteps.cpp
+++ Minigin/GraphSteps.cpp
@@ -1,7 +1,19 @@
 #include "GraphSteps.h"
+#include <chrono>
+
 #include "..\3rdParty\imgui-1.90.4\backends\imgui_impl_opengl3.h"
 #include "..\3rdParty\imgui-1.90.4\backends\imgui_impl_sdl2.h"
-#include <chrono>
+#include "..\3rdParty\imgui-1.90.4\plot-master\imgui_plot.h"
+
+GraphSteps::GraphSteps(std::shared_ptr<dae::GameObject>& pOwner):
+	ObjectComponent(pOwner)
+{
+	m_arrInt.resize(sizeArr); 
+	m_arrClass.resize(sizeArr);
+
+	m_plotInt.frame_size = { 250,125 };
+	m_plotClass.frame_size = m_plotInt.frame_size;
+}
 
 void GraphSteps::Render() const
 {
@@ -18,6 +30,10 @@ void GraphSteps::Render() const
 			{
 				m_startLoadingInt = true;
 			}
+			if (m_timesInt.size() > 0)
+			{
+				ImGui::Plot(" ", m_plotInt);
+			}
 		}
 		else ImGui::Text(m_waitText.c_str());
 		if(!m_isLoadingClass)
@@ -25,6 +41,10 @@ void GraphSteps::Render() const
 			if (ImGui::Button("Trash the cache with class"))
 			{
 				m_startLoadingClass = true;
+			}
+			if (m_timesClass.size() > 0)
+			{
+				ImGui::Plot(" ", m_plotClass);
 			}
 		}
 		else ImGui::Text(m_waitText.c_str());
@@ -39,29 +59,68 @@ void GraphSteps::Update(float)
 {
 	if (m_startLoadingInt)
 	{
+		m_timesInt.clear();
+
 		m_startLoadingInt = false;
 		m_isLoadingInt = true;
+
+		CalculateLoopInt();
 	}
 	if (m_startLoadingClass)
 	{
+		m_timesClass.clear();
+
 		m_startLoadingClass = false;
 		m_isLoadingClass = true;
+
+		CalculateLoopClass();
 	}
 }
 
 void GraphSteps::CalculateLoopInt()
 {
-	for (int step{1}; step <= 1024; step *= 2)
+	for(int times{}; times < m_samples; ++times)
 	{
-		for (int stepsize{ step }; stepsize <= 1024; stepsize *= 2)
+		//for (int step{ 1 }; step <= 1024; step *= 2)
 		{
-			auto start = std::chrono::high_resolution_clock::now();
-			for (int i = 0; i < m_arrInt.size(); i += stepsize)
+			for (int stepsize{ 1 }; stepsize <= 1024; stepsize *= 2)
 			{
-				m_arrInt[i] *= 2;
+				auto start = std::chrono::high_resolution_clock::now();
+				for (int i = 0; i < m_arrInt.size(); i += stepsize)
+				{
+					m_arrInt[i] *= 2;
+				}
+				auto end = std::chrono::high_resolution_clock::now();
+				auto elapsedTime = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+
+				m_timesInt.emplace_back(elapsedTime);
 			}
-			auto end = std::chrono::high_resolution_clock::now();
-			//auto elapsedTime = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
 		}
 	}
+
+	m_isLoadingInt = false;
+}
+
+void GraphSteps::CalculateLoopClass()
+{
+	for (int times{}; times < m_samples; ++times)
+	{
+		//for (int step{ 1 }; step <= 1024; step *= 2)
+		{
+			for (int stepsize{ 1 }; stepsize <= 1024; stepsize *= 2)
+			{
+				auto start = std::chrono::high_resolution_clock::now();
+				for (int i = 0; i < m_arrClass.size(); i += stepsize)
+				{
+					m_arrClass[i].Function();
+				}
+				auto end = std::chrono::high_resolution_clock::now();
+				auto elapsedTime = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+
+				m_timesClass.emplace_back(elapsedTime);
+			}
+		}
+	}
+
+	m_isLoadingClass = false;
 }
--- Minigin/GraphSteps.h
+++ Minigin/GraphSteps.h
@@ -4,6 +4,10 @@
 #include "Transform.h"
 #include "ObjectComponent.h"
 
+#include "..\3rdParty\imgui-1.90.4\backends\imgui_impl_opengl3.h"
+#include "..\3rdParty\imgui-1.90.4\backends\imgui_impl_sdl2.h"
+#include "..\3rdParty\imgui-1.90.4\plot-master\imgui_plot.h"
+
 class TestObject
 {
 	dae::Transform* m_pLocal;
@@ -15,22 +19,26 @@ public:
 class GraphSteps : public ObjectComponent
 {
 public:
-	GraphSteps(std::shared_ptr<dae::GameObject>& pOwner): ObjectComponent(pOwner) { m_arrInt.resize(sizeArr); m_arrClass.resize(sizeArr); };
+	GraphSteps(std::shared_ptr<dae::GameObject>& pOwner);
 	~GraphSteps() = default;
+	GraphSteps(const GraphSteps& copy) = delete;
+	GraphSteps(GraphSteps&& move) = delete;
+	GraphSteps& operator=(const GraphSteps& toCopy) = delete;
+	GraphSteps& operator=(GraphSteps&& toMove) = delete;
 
 	void Render() const;
 	void Update(float deltaTime);
 
 private:
-	mutable int m_samples{ 100 };
+	mutable int m_samples{ 2 };
 
 	const int sizeArr{ 10000000 };
 
 	std::vector<int> m_arrInt{};
 	std::vector<TestObject> m_arrClass{};
 
-	std::vector<float> m_timesInt{};
-	std::vector<float> m_timesClass{};
+	std::vector<long long> m_timesInt{};
+	std::vector<long long> m_timesClass{};
 
 	bool m_isLoadingInt{ false };
 	bool m_isLoadingClass{ false };
@@ -39,6 +47,9 @@ private:
 	mutable bool m_startLoadingClass{ false };
 
 	const std::string m_waitText{ "Wait for it..." };
+
+	ImGui::PlotConfig m_plotInt{};
+	ImGui::PlotConfig m_plotClass{};
 
 	void CalculateLoopInt();
 	void CalculateLoopClass();
